8b9661c8f4e240b8b81370ed365e36677c083c6d
Gabby O'Dell Vanner <grodell@wpi.edu>
2025-04-07 20:26:19 -0400

    Add files via upload

---
+++ fanuc/kinematics/include/kinematics/kinematics.h
@@ -0,0 +1,36 @@
+#ifndef GAZEBO_PLUGINS__KINEMATICS_PLUGIN_H
+#define GAZEBO_PLUGINS__KINEMATICS_PLUGIN_H
+
+// ROS
+#include <ros/ros.h>
+
+// Gazebo
+#include <gazebo/common/Plugin.hh>
+#include <gazebo/common/Time.hh>
+#include <gazebo/physics/Joint.hh>
+#include <gazebo/physics/Link.hh>
+#include <gazebo/physics/Model.hh>
+#include <gazebo/physics/World.hh>
+#include <gazebo/physics/physics.hh>
+
+#include <gazebo_ros/conversions/builtin_interfaces.hpp>
+#include <gazebo_ros/conversions/geometry_msgs.hpp>
+#include <gazebo_ros/node.hpp>
+
+// MoveIt
+#include <moveit/kinematics_base/kinematics_base.h>
+#include <moveit/robot_model/robot_model.h>
+#include <moveit/robot_state/robot_state.h>
+#include <moveit_msgs/GetPositionFK.h>
+#include <moveit_msgs/GetPositionIK.h>
+#include <moveit_msgs/KinematicSolverInfo.h>
+#include <moveit_msgs/MoveItErrorCodes.h>
+
+
+namespace fanuc_kinematics {
+
+
+
+}
+
+#endif
